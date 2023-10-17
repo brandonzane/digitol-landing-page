44208a576058456df3cc1e957295a63c751f5158
brandonzane <bmushori@gmail.com>
2023-10-17 15:48:45 +0200

    Added email functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Analytics from './components/Analytics';
-import Cards from './components/Cards';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Newsletter from './components/Newsletter';
+import React from "react";
+import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
 
 function App() {
   return (
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,45 +1,79 @@
-import React from 'react';
-import Single from '../assets/single.png'
-import Double from '../assets/double.png'
-import Triple from '../assets/triple.png'
+import React from "react";
+import Single from "../assets/single.png";
+import Double from "../assets/double.png";
+import Triple from "../assets/triple.png";
 
 const Cards = () => {
   return (
-    <div className='w-full py-[10rem] px-4 bg-white'>
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
-          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
-              <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
-              </div>
-              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+    <div className="w-full py-[2rem] px-4 bg-white">
+      <h1 className="text-black text-2xl font-bold text-center pb-20">
+        Website Pricing
+      </h1>
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl flex flex-col p- my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Single}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Starter</h2>
+          <p className="text-center text-4xl font-bold">$500</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">Optimised Website</p>
+            <p className="py-2 border-b mx-8">
+              {" "}
+              Custom Analytics & Google Analytics
+            </p>
+            <p className="py-2 border-b mx-8">Custom Design</p>
+            <p className="py-2 border-b mx-8">2 Design Revisions</p>
+            <p className="py-2 border-b mx-8">Easy to use CMS</p>
           </div>
-          <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
-              <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
-              </div>
-              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            Get Started
+          </button>
+        </div>
+        <div className="w-full shadow-xl bg-white flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-transparent"
+            src={Double}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Premium</h2>
+          <p className="text-center text-4xl font-bold">$1500</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">Online Store</p>
+            <p className="py-2 border-b mx-8 mt-8">
+              Additional Features eg. Bookings & User accounts
+            </p>
+            <p className="py-2 border-b mx-8">Emails & Support</p>
+            <p className="py-2 border-b mx-8">CMS or Custom Code</p>
+            <p className="py-2 border-b mx-8">Staff Training</p>
           </div>
-          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
-              <div className='text-center font-medium'>
-                  <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                  <p className='py-2 border-b mx-8'>1 Granted User</p>
-                  <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
-              </div>
-              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+          <button className="bg-black text-white w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            Start Trial
+          </button>
+        </div>
+        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Triple}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Enterprise</h2>
+          <p className="text-center text-lg font-bold">Contact Us</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">Everything in Premium</p>
+            <p className="py-2 border-b mx-8">Hourly Backup</p>
+            <p className="py-2 border-b mx-8">Security and Monitoring</p>
+            <p className="py-2 border-b mx-8">Unlimited Design Revisions</p>
+            <p className="py-2 border-b mx-8">Premium Customer Support</p>
+            <p className="py-2 border-b mx-8">Premium Customer Support</p>
+            <p className="py-2 border-b mx-8">Conversion Tracking</p>
           </div>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+            Contact Us
+          </button>
+        </div>
       </div>
     </div>
   );
